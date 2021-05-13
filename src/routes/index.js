c71339c0794a945da71e3c5e43ff0d93c6367a0c
mariajmr1202 <mariajmr1202@gmail.com>
2021-05-13 01:37:59 -0400

    Cambios de url home

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import getHash from "../utils/getHash";
 import resolveRoutes from "../utils/resolveRoutes";
 
 const routes = {
-  "/": Home,
+  "/cientifico": Home,
   "/:id": Character,
   "/contact": "Contact",
 };
